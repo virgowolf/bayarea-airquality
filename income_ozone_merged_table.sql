94d4933745a92bcfab4c14b6ef523f249ac7d233
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 <christopherkilkes@gmail.com>
2024-05-22 14:13:27 -0700

    Commit SQL Files

--- income_ozone_merged_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE income_ozone_merged_table (
-    city_index INT,
-	city VARCHAR(100),
-    month DATE,
-    ozone_median NUMERIC,
-	median_income_2020 NUMERIC,
-	PRIMARY KEY (city, city_index, month),
-	FOREIGN KEY (city, city_index, month) REFERENCES city_master_table(city, city_index, month)
-);
