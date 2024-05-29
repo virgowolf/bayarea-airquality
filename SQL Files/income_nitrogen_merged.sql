aa95adf4e1b4b15dc1c70b72f65c4706c1ec8608
Celina Kamler <156024981+virgowolf@users.noreply.github.com>
2024-05-28 22:29:03 -0700

    Delete old SQL Files directory

--- SQL Files/income_nitrogen_merged.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE income_nitrogen_merged_table (
-    city_index INT,
-	city VARCHAR(100),
-    month DATE,
-    nitrogen_max NUMERIC,
-	median_income_2020 NUMERIC,
-	PRIMARY KEY (city, city_index, month),
-	FOREIGN KEY (city, city_index, month) REFERENCES city_master_table(city, city_index, month)
-);
